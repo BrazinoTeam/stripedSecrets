6d0331e40a480ad2c7bb430e6218eddb8f156a2f
hilsonV <hilsonvilliam@gmail.com>
2024-07-10 16:15:37 +0300

    update 10.07

--- stripedSecrets/App/Flow/TabBar/Bonus/BonusVC.swift
+++ stripedSecrets/App/Flow/TabBar/Bonus/BonusVC.swift
@@ -3,10 +3,14 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 class BonusVC: UIViewController {
     
-    
+    private var fullScreenView: UIView?
+    private var bonusPoints: [Int] = [100, 200, 300, 400, 500, 600, 1000, 1200, 1500]
+    private var isTime: Bool = true
+
     private var contentView: BonusView {
         view as? BonusView ?? BonusView()
     }
@@ -16,14 +20,214 @@ class BonusVC: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupButtonActions()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        goDailyScreen()
         configureLabel()
     }
     
     private func configureLabel() {
         contentView.scoreLabel.text = "\(UserDef.shared.scorePoints)"
     }
+    
+    
+    private func setupButtonActions() {
+        for button in contentView.buttons {
+            button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        }
+        contentView.btnBonus.addTarget(self, action: #selector(bonusTapped) , for: .touchUpInside)
+    }
+    
+    @objc private func buttonTapped(_ sender: UIButton) {
+        for button in contentView.buttons {
+            button.layer.shadowColor = UIColor.clear.cgColor
+            button.layer.shadowOpacity = 0
+            button.layer.shadowRadius = 0
+            button.layer.shadowOffset = CGSize(width: 0, height: 0)
+        }
+        
+        sender.layer.shadowColor = UIColor.black.withAlphaComponent(0.7).cgColor
+        sender.layer.shadowOpacity = 1
+        sender.layer.shadowRadius = 12
+        sender.layer.shadowOffset = CGSize(width: 0, height: 16)
+        print("Button \(sender.tag) tapped")
+        contentView.btnBonus.configureButton(withTitle: "Get Bonus", font: .customFont(font: .joti, style: .regular, size: 24), titleColor: .cDarkRed, normalImage: .btnActivity, highlightedImage: .btnActivityTapped)
+        contentView.btnBonus.isEnabled = true
+
+    }
+    
+    @objc private func bonusTapped() {
+        let randomBonus = bonusPoints.randomElement() ?? 0
+        UserDef.shared.scorePoints += randomBonus
+        presentModalView(points: randomBonus)
+    }
+    
+    private func presentModalView(points: Int) {
+        if fullScreenView == nil {
+            fullScreenView = UIView(frame: self.view.bounds)
+            fullScreenView!.backgroundColor = .black.withAlphaComponent(0.85)
+            fullScreenView!.alpha = 0
+            
+            let viewContainer = UIView()
+            viewContainer.backgroundColor = .clear
+            viewContainer.layer.borderWidth = 1
+            viewContainer.layer.borderColor = UIColor.cLightYellow.cgColor
+            viewContainer.layer.cornerRadius = 16
+            fullScreenView!.addSubview(viewContainer)
+            
+            let bgImage = UIImageView(image: .contModel)
+            bgImage.contentMode = .scaleToFill
+            bgImage.clipsToBounds = true
+            viewContainer.addSubview(bgImage)
+            
+            let titleLabel = UILabel.createLabel(withText: "Congratilations!", font: .customFont(font: .joti, style: .regular, size: 28), textColor: .cLightYellow, paragraphSpacing: 0, lineHeightMultiple: 1)
+            titleLabel.numberOfLines = 0
+            titleLabel.textAlignment = .center
+            viewContainer.addSubview(titleLabel)
+            
+            let imageBonusView = UIImageView(image: .imgPointsBonus)
+            imageBonusView.contentMode = .scaleAspectFit
+            viewContainer.addSubview(imageBonusView)
+            
+            let subTitleLabelView = UILabel.createLabel(withText: "You won today", font: .customFont(font: .sofia, style: .light, size: 16), textColor: .white, paragraphSpacing: 0, lineHeightMultiple: 1.17)
+            subTitleLabelView.numberOfLines = 0
+            subTitleLabelView.textAlignment = .center
+            viewContainer.addSubview(subTitleLabelView)
+            
+            let pointsLabel = UILabel.createLabel(withText: "+ \(points) Points", font: .customFont(font: .joti, style: .regular, size: 20), textColor: .white, paragraphSpacing: 0, lineHeightMultiple: 1.17)
+            pointsLabel.numberOfLines = 0
+            pointsLabel.textAlignment = .center
+            viewContainer.addSubview(pointsLabel)
+
+            let thanksBtn = UIButton()
+            thanksBtn.configureButton(withTitle: "OK", font: .customFont(font: .joti, style: .regular, size: 24), titleColor: .cDarkRed, normalImage: .btnActivity, highlightedImage: .btnActivityTapped)
+            thanksBtn.addTarget(self, action: #selector(tappedCloseBuy), for: .touchUpInside)
+            fullScreenView!.addSubview(thanksBtn)
+            
+            viewContainer.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.centerY.equalToSuperview()
+                make.height.equalTo(512)
+                make.width.equalTo(312)
+            }
+            
+            bgImage.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+            
+            titleLabel.snp.makeConstraints { make in
+                make.centerX.equalTo(imageBonusView)
+                make.top.equalToSuperview().offset(36)
+            }
+            
+            imageBonusView.snp.makeConstraints { make in
+                make.centerX.equalTo(viewContainer)
+                make.top.equalTo(titleLabel.snp.bottom).offset(16)
+                make.size.equalTo(248)
+            }
+            
+            subTitleLabelView.snp.makeConstraints { make in
+                make.centerX.equalTo(imageBonusView)
+                make.top.equalTo(imageBonusView.snp.bottom).offset(16)
+            }
+            
+            pointsLabel.snp.makeConstraints { make in
+                make.centerX.equalTo(imageBonusView)
+                make.top.equalTo(subTitleLabelView.snp.bottom).offset(8)
+            }
+        
+            thanksBtn.snp.makeConstraints { make in
+                make.top.equalTo(pointsLabel.snp.bottom).offset(16)
+                make.centerX.equalToSuperview()
+                make.width.equalTo(260)
+                make.height.equalTo(52)
+            }
+            
+            self.view.addSubview(fullScreenView!)
+        }
+        UIView.animate(withDuration: 0.5, animations: {
+            self.fullScreenView!.alpha = 1
+        })
+    }
+    
+    @objc func tappedCloseBuy() {
+        UserDef.shared.lastBonusDate = Date()
+        UIView.animate(withDuration: 0.5, animations: {
+            self.fullScreenView?.alpha = 0
+        }) { _ in
+            self.fullScreenView?.removeFromSuperview()
+            self.fullScreenView = nil
+            self.goDailyScreen()
+            self.configureLabel()
+        }
+    }
+}
+
+extension BonusVC {
+    
+    func goDailyScreen() {
+        if let lastVisitDate = UserDef.shared.lastBonusDate {
+            let calendar = Calendar.current
+            if let hours = calendar.dateComponents([.hour], from: lastVisitDate, to: Date()).hour, hours < 24 {
+                isTime = true
+                contentView.timerView.isHidden = false
+                startCountdownTimer()
+            } else {
+                isTime = false
+                contentView.timerView.isHidden = true
+            }
+        }
+    }
+    
+    private func createAttributedString(text: String) -> NSAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.paragraphSpacing = 0
+        paragraphStyle.lineHeightMultiple = 1.03
+        
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.customFont(font: .joti, style: .regular, size: 64),
+            .foregroundColor: UIColor.cLightYellow,
+            .paragraphStyle: paragraphStyle
+        ]
+        
+        return NSAttributedString(string: text, attributes: attributes)
+    }
+    
+    func startCountdownTimer() {
+        let calendar = Calendar.current
+        
+        guard let lastVisitDate = UserDef.shared.lastBonusDate,
+              let targetDate = calendar.date(byAdding: .day, value: 1, to: lastVisitDate) else {
+            return
+        }
+        
+        let now = Date()
+        if now < targetDate {
+            let timeRemaining = calendar.dateComponents([.hour, .minute, .second], from: now, to: targetDate)
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+                guard let self = self else {
+                    timer.invalidate()
+                    return
+                }
+                
+                let now = Date()
+                if now >= targetDate {
+                    UserDefaults.standard.set(now, forKey: "LastVisitDate")
+                    self.dismiss(animated: true, completion: nil)
+                    timer.invalidate()
+                } else {
+                    let timeRemaining = calendar.dateComponents([.hour, .minute, .second], from: now, to: targetDate)
+                    let timeString = String(format: "%02d:%02d:%02d", timeRemaining.hour ?? 0, timeRemaining.minute ?? 0, timeRemaining.second ?? 0)
+                    self.contentView.otherCountLabel.attributedText = self.createAttributedString(text: timeString)
+                }
+            }
+        } else {
+            UserDefaults.standard.set(now, forKey: "LastVisitDate")
+        }
+    }
+    
 }
 
--- stripedSecrets/App/Flow/TabBar/Bonus/BonusView.swift
+++ stripedSecrets/App/Flow/TabBar/Bonus/BonusView.swift
@@ -7,6 +7,10 @@ import SnapKit
 
 class BonusView: UIView {
     
+    private (set) var bonusView: UIView = {
+        let view = UIView()
+        return view
+    }()
     
     private (set) var bgImage: UIImageView = {
         let imageView = UIImageView()
@@ -38,6 +42,103 @@ class BonusView: UIView {
         return label
     }()
     
+    private (set) var titleLabel: UILabel = {
+        let label = UILabel.createLabel(withText: "Daily Bonus", font: .customFont(font: .joti, style: .regular, size: 28), textColor: .cLightYellow, paragraphSpacing: 1, lineHeightMultiple: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private (set) var buttonImages: [UIImage] = [
+           .imgBonusCart01, .imgBonusCart02, .imgBonusCart03,
+           .imgBonusCart04, .imgBonusCart05, .imgBonusCart06,
+           .imgBonusCart07, .imgBonusCart08, .imgBonusCart09
+       ]
+       
+    
+    private(set) var buttons: [UIButton] = {
+        var buttons = [UIButton]()
+        for i in 0..<9 {
+            let button = UIButton(type: .custom)
+            button.tag = i
+            buttons.append(button)
+        }
+        return buttons
+    }()
+    
+    private(set) lazy var btnBonus: UIButton = {
+        let button = UIButton()
+        button.configureButton(withTitle: "Get Bonus", font: .customFont(font: .joti, style: .regular, size: 24), titleColor: .cDarkRed, normalImage: .btnActivityLocked, highlightedImage: .btnActivityLocked)
+        button.layer.shadowColor = UIColor.cDarkRed.withAlphaComponent(0.4).cgColor
+        button.layer.shadowOpacity = 1
+        button.layer.shadowRadius = 2
+        button.layer.shadowOffset = CGSize(width: 2, height: 4)
+        button.isEnabled = false
+        return button
+    }()
+    
+    private (set) var timerView: UIView = {
+        let view = UIView()
+        view.isHidden = true
+        return view
+    }()
+    
+    private (set) var otherBgImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .bgOther
+        return imageView
+    }()
+    
+    private (set) var otherContScoreView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cDarkRed
+        view.layer.borderColor = UIColor.cLightYellow.cgColor
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 26
+        view.layer.shadowColor = UIColor.cLightYellow.cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowRadius = 8
+        view.layer.shadowOffset = CGSize(width: 0, height: 0)
+        return view
+    }()
+    
+    private (set) var otherImgPoints: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .imgPoints
+        return imageView
+    }()
+    
+    private (set) var otherScoreLabel: UILabel = {
+        let label = UILabel.createLabel(withText: "\(UserDef.shared.scorePoints)", font: .customFont(font: .joti, style: .regular, size: 20), textColor: .white, paragraphSpacing: 1, lineHeightMultiple: 1)
+        return label
+    }()
+    
+    private (set) var otherTitleLabel: UILabel = {
+        let label = UILabel.createLabel(withText: "Daily Bonus", font: .customFont(font: .joti, style: .regular, size: 28), textColor: .cLightYellow, paragraphSpacing: 1, lineHeightMultiple: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private (set) var otherSubTitleLabel: UILabel = {
+        let label = UILabel.createLabel(withText: "The next bonus will be available through:", font: .customFont(font: .sofia, style: .light, size: 14), textColor: .cLightYellow, paragraphSpacing: 1, lineHeightMultiple: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private (set) var otherCountLabel: UILabel = {
+        let label = UILabel.createLabel(withText: "", font: .customFont(font: .joti, style: .regular, size: 64), textColor: .cLightYellow, paragraphSpacing: 1, lineHeightMultiple: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private (set) var otherImgTiger: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .imgTigerModal
+        return imageView
+    }()
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -49,15 +150,32 @@ class BonusView: UIView {
     }
     
     private func setupUI() {
-
-        [bgImage, contScoreView] .forEach(addSubview(_:))
+        addSubview(bonusView)
+        bonusView.addSubview(bgImage)
+        bonusView.addSubview(contScoreView)
+        bonusView.addSubview(titleLabel)
+        buttons.forEach { bonusView.addSubview($0) }
+        bonusView.addSubview(btnBonus)
         contScoreView.addSubview(imgPoints)
         contScoreView.addSubview(scoreLabel)
-
+        for button in buttons {
+                    if button.tag < buttonImages.count {
+                        button.setImage(buttonImages[button.tag], for: .normal)
+                    }
+                }
+        
+        addSubview(timerView)
+        [otherBgImage, otherContScoreView, otherTitleLabel, otherSubTitleLabel, otherCountLabel, otherImgTiger] .forEach(timerView.addSubview(_:))
+        otherContScoreView.addSubview(otherImgPoints)
+        otherContScoreView.addSubview(otherScoreLabel)
     }
     
     private func setupConstraints() {
-     
+        
+        bonusView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
         bgImage.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
@@ -78,6 +196,79 @@ class BonusView: UIView {
             make.centerY.equalTo(imgPoints)
             make.left.equalTo(imgPoints.snp.right).offset(20)
         }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(contScoreView.snp.bottom).offset(40)
+        }
+        
+        let buttonWidth = 112
+        let buttonHeight = 112
+        let spacing = 8
+        let heightSpacing = 16
+        
+        for (index, button) in buttons.enumerated() {
+            let row = index / 3
+            let column = index % 3
+            button.snp.makeConstraints { make in
+                make.width.equalTo(buttonWidth)
+                make.height.equalTo(buttonHeight)
+                make.top.equalTo(titleLabel.snp.bottom).offset(36 + (row * (buttonHeight + heightSpacing)))
+                make.centerX.equalToSuperview().offset((buttonWidth + spacing) * (column - 1))
+            }
+        }
+        
+        btnBonus.snp.makeConstraints { make in
+            make.top.equalTo(buttons.last!.snp.bottom).offset(36)
+            make.left.equalToSuperview().offset(16)
+            make.right.equalToSuperview().offset(-16)
+            make.height.equalTo(44)
+        }
+        
+        timerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        otherBgImage.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        otherContScoreView.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.width.equalTo(140)
+            make.top.equalToSuperview().offset(60)
+            make.right.equalToSuperview().offset(-20)
+        }
+        
+        otherImgPoints.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(10)
+            make.centerY.equalToSuperview()
+        }
+        
+        otherScoreLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(imgPoints)
+            make.left.equalTo(imgPoints.snp.right).offset(20)
+        }
+        
+        otherTitleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(contScoreView.snp.bottom).offset(40)
+        }
+        
+        otherSubTitleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(otherTitleLabel.snp.bottom).offset(4)
+        }
+        
+        otherCountLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(otherSubTitleLabel.snp.bottom).offset(36)
+            make.height.equalTo(50)
+        }
+        
+        otherImgTiger.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(otherCountLabel.snp.bottom).offset(36)
+        }
     }
 }
-
